fc93edcd0d56255905503e8faf8ea8b326a3e4e9
John <37167651+KujoKnight@users.noreply.github.com>
2019-10-02 17:08:03 -0400

    Created prototype for song tempo recognition (Used outside script for testing purposes)

---
+++ TempoTapPrototype/Assets/Scripts/MusicTempo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTempo : MonoBehaviour
+{
+    void Start()
+    {
+        AudioProcessor processor = FindObjectOfType<AudioProcessor>();
+        processor.onBeat.AddListener(onOnbeatDetected);
+        processor.onSpectrum.AddListener(onSpectrum);
+    }
+    void onOnbeatDetected()
+    {
+        Debug.Log("Beat!!!");
+    }
+    void onSpectrum(float[] spectrum)
+    {
+        for (int i = 0; i < spectrum.Length; ++i)
+        {
+            Vector3 start = new Vector3(i, 0, 0);
+            Vector3 end = new Vector3(i, spectrum[i], 0);
+            Debug.DrawLine(start, end);
+        }
+    }
+}
